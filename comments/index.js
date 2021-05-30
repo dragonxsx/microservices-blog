639c823e7dc88843852f134cb9e2c7b8f62e623f
Nguyen Thanh Long <dragon.sunshine@gmail.com>
2021-05-30 17:27:18 +0700

    Rendering posts

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {randomBytes} = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
