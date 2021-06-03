a41960ab14e9fe083fbbfaf2407a254c07fa57b9
Nguyen Thanh Long <longnt@ql6625.fr>
2021-06-03 09:58:39 +0700

    Implement Event Sync

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,20 +5,24 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', async (req, res) => {
     const event = req.body;
 
-    try {
-        await axios.post('http://localhost:4000/events', event);
-        await axios.post('http://localhost:4001/events', event);
-        await axios.post('http://localhost:4002/events', event);
-        await axios.post('http://localhost:4003/events', event);
-    } catch(e) {
-        console.log(e);
-    }
+    events.push(event);
+
+    await axios.post('http://localhost:4000/events', event).catch(e => console.log(e));
+    await axios.post('http://localhost:4001/events', event).catch(e => console.log(e));
+    await axios.post('http://localhost:4002/events', event).catch(e => console.log(e));
+    await axios.post('http://localhost:4003/events', event).catch(e => console.log(e));
 
     res.send({status: 'OK'});
-})
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
+});
 
 app.listen(4005, () => {
     console.log('Listening on 4005');
